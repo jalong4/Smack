3b73a684ff36c0077c69c8cdb6c414e36217b447
Jim Long <jalong4@hotmail.com>
2018-03-17 21:05:49 -0700

    Add UX for LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Jim Long on 3/17/18.
+//  Copyright © 2018 Jim Long. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    // Outlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Jim Long on 3/17/18.
+//  Copyright © 2018 Jim Long. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+
+let TO_LOGIN = "toLogin"
